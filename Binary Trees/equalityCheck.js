9f87c33b0e1854a13cf580b119447683577baddc
Deanna Dunne <deanna.dunne@gmail.com>
2020-10-03 16:37:40 -0400

    problem 10/3

---
+++ Binary Trees/equalityCheck.js
@@ -0,0 +1,7 @@
+/*
+Given two binary trees, write a function that checks if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and corresponding nodes have the same values.
+*/
+
+function equalityCheck(head1, head2) {}
